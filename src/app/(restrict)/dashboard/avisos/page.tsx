d35fe89f6c5582e89c0fdd78fbdbc0da7098ba35
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2025-04-04 14:53:07 -0300

    avisos

--- src/app/(restrict)/dashboard/avisos/page.tsx
+++ src/app/(restrict)/dashboard/avisos/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { ptBR } from "date-fns/locale";
 import { DndContext, closestCenter } from "@dnd-kit/core";
 import {
   SortableContext,
@@ -10,13 +11,49 @@ import {
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Button } from "@/components/ui/button";
-import { Calendar, Check, Info, PlusCircle } from "lucide-react";
-import Link from "next/link";
+
+import {
+  Calendar as CalendarIcon,
+  Check,
+  Info,
+  PlusCircle,
+} from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+
+import { format } from "date-fns";
+
+import { cn } from "@/lib/utils";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 function DraggableCard({
   item,
 }: {
-  item: { id: string; title: string; description: string };
+  item: { id: string; title: string; description: string; date?: Date };
 }) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: item.id });
@@ -27,81 +64,196 @@ function DraggableCard({
       {...attributes}
       {...listeners}
       style={{ transform: CSS.Transform.toString(transform), transition }}
-      className="w-full min-h-24 bg-white rounded-md p-3 shadow cursor-grab flex flex-col"
+      className="w-full min-h-24 shadow-2xl bg-white rounded-md p-5 cursor-grab flex flex-col"
     >
-      <h4 className="font-bold">{item.title}</h4>
-      <p className="text-sm text-gray-600">{item.description}</p>
+      <h4 className="font-bold text-blue-500 border-b text-center border-blue-200 pb-3">
+        {item.title}
+      </h4>
+      <p className="text-sm text-gray-600 pt-3 text-justify mb-5">
+        {item.description}
+      </p>
+      {item.date && (
+        <p className="text-xs text-blue-500 pt-2 text-center pb-2 underline">
+          Agendado para: {format(new Date(item.date), "PPP", { locale: ptBR })}
+        </p>
+      )}
     </div>
   );
 }
 
 export default function Page() {
-  const [newAlerts, setNewAlerts] = useState([
-    { id: "1", title: "Novo Aviso 1", description: "Visitar Timon" },
-    { id: "2", title: "Novo Aviso 2", description: "Instalar SCPI 9 VM 18" },
-  ]);
+  const [date, setDate] = useState<Date | undefined>();
+  const [userCity, setUserCity] = useState("");
+  const [newAlerts, setNewAlerts] = useState<
+    { id: string; title: string; description: string; date?: Date }[]
+  >([]);
+  const [weeklyAlerts, setWeeklyAlerts] = useState<
+    { id: string; title: string; description: string; date?: Date }[]
+  >([]);
 
-  const [weeklyAlerts, setWeeklyAlerts] = useState([
-    {
-      id: "4",
-      title: "Aviso da Semana 1",
-      description: "Resumo do aviso semanal 1.",
-    },
-    {
-      id: "5",
-      title: "Aviso da Semana 2",
-      description: "Resumo do aviso semanal 2.",
-    },
-    {
-      id: "6",
-      title: "Aviso da Semana 3",
-      description: "Resumo do aviso semanal 3.",
-    },
-    {
-      id: "7",
-      title: "Aviso da Semana 4",
-      description: "Resumo do aviso semanal 4.",
-    },
-    {
-      id: "8",
-      title: "Aviso da Semana 5",
-      description: "Resumo do aviso semanal 5.",
-    },
-  ]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [user, setUser] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDragEnd = (event: any) => {
     const { active, over } = event;
     if (!over || active.id === over.id) return;
-
     setNewAlerts((items) => {
-      const oldIndex = items.findIndex((i) => i.id === active.id);
-      const newIndex = items.findIndex((i) => i.id === over.id);
-      return arrayMove(items, oldIndex, newIndex);
-    });
-
-    setWeeklyAlerts((items) => {
       const oldIndex = items.findIndex((i) => i.id === active.id);
       const newIndex = items.findIndex((i) => i.id === over.id);
       return arrayMove(items, oldIndex, newIndex);
     });
   };
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (!title.trim() || !description.trim() || !user.trim()) return;
+    setNewAlerts([
+      ...newAlerts,
+      {
+        id: Date.now().toString(),
+        title: `${title} - ${user}  (${userCity})`,
+        description,
+        date,
+      },
+    ]);
+    setTitle("");
+    setUser("");
+    setUserCity("");
+    setDescription("");
+    toast.info("Você tem um novo aviso!");
+    setIsOpen(false);
+  };
+
   return (
     <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
       <div className="mt-5">
-        <div className="border-b pb-5 flex items-center justify-between">
+        <div className="pb-5 flex items-center justify-between">
           <h2 className="font-bold text-xl flex items-center gap-2">
             <Info /> Avisos
           </h2>
-          <Link href={"/dashboard/avisos/novo"}>
-            <Button variant={"default"}>
-              <PlusCircle /> Novo Aviso
-            </Button>
-          </Link>
+          <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+              <Button variant="default">
+                <PlusCircle /> Novo Aviso
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Novo Aviso</DialogTitle>
+                <form onSubmit={handleSubmit}>
+                  <div className="mt-5">
+                    <Label className="mb-2" htmlFor="tipo-aviso">
+                      Tipo do Aviso
+                    </Label>
+                    <Select onValueChange={setTitle} value={title}>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Selecione um tipo" />
+                      </SelectTrigger>
+                      <SelectContent id="tipo-aviso">
+                        <SelectItem value="Visita Técnica">
+                          Visita Técnica
+                        </SelectItem>
+                        <SelectItem value="Instalação">Instalação</SelectItem>
+                        <SelectItem value="Treinamento">Treinamento</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="mt-5">
+                    <Label className="mb-2" htmlFor="usuario">
+                      Usuário
+                    </Label>
+                    <Select onValueChange={setUser} value={user}>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Selecione um usuário" />
+                      </SelectTrigger>
+                      <SelectContent id="usuario">
+                        <SelectItem value="Rodrigo Tutz">
+                          Rodrigo Tutz
+                        </SelectItem>
+                        <SelectItem value="João Manoel">João Manoel</SelectItem>
+                        <SelectItem value="Antonio Mesquita">
+                          Antonio Mesquita
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="mt-5">
+                    <Label className="mb-2" htmlFor="cliente">
+                      Cliente / Cidade
+                    </Label>
+                    <Select onValueChange={setUserCity} value={userCity}>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Selecione um cliente ou cidade" />
+                      </SelectTrigger>
+                      <SelectContent id="cliente">
+                        <SelectItem value="Picos">Picos</SelectItem>
+                        <SelectItem value="Fronteiras">Fronteiras</SelectItem>
+                        <SelectItem value="São Luiz">São Luiz</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="mt-5">
+                    <Label className="mb-2" htmlFor="description">
+                      Descrição
+                    </Label>
+                    <Input
+                      type="text"
+                      id="description"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      placeholder="Descrição do aviso"
+                    />
+                  </div>
+
+                  <div className="mt-5">
+                    <Label className="mb-2">Agendamento</Label>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full justify-start text-left font-normal",
+                            !date && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {date ? (
+                            format(date, "PPP", { locale: ptBR })
+                          ) : (
+                            <span>Selecione uma Data</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={date}
+                          locale={ptBR}
+                          onSelect={setDate}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+
+                  <div className="mt-5">
+                    <Button type="submit" className="w-full">
+                      Salvar Aviso
+                    </Button>
+                  </div>
+                </form>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
 
-        <div className="p-5 flex justify-between gap-10">
-          <div className="min-h-[500px] w-1/4 rounded-md border-2 p-5 flex flex-col gap-10">
+        <div className="p-5 flex justify-between gap-10 ">
+          <div className="min-h-[500px] w-2/5 rounded-md border-2 p-5 flex flex-col gap-10">
             <h3 className="text-center text-lg font-bold flex items-center justify-center gap-1">
               <Check size={18} /> Novos avisos
             </h3>
@@ -119,7 +271,7 @@ export default function Page() {
 
           <div className="w-full min-h-96 rounded-md border-2 p-5 flex flex-col gap-10">
             <h3 className="text-center font-bold text-lg flex gap-2 items-center">
-              <Calendar /> Todos os Avisos da Semana:
+              <CalendarIcon /> Todos os Avisos da Semana:
             </h3>
             <SortableContext
               items={weeklyAlerts}
