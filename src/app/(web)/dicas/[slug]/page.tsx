a7d4f7886a21d0bfcaf0c85c0fd260739ad6200f
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2025-04-09 15:14:02 -0300

    ajuste de layout

--- src/app/(web)/dicas/[slug]/page.tsx
+++ src/app/(web)/dicas/[slug]/page.tsx
@@ -23,8 +23,8 @@ export default async function Page({
   const cleanHtml = sanitizeHTML(tip.content);
 
   return (
-    <div className="flex items-start gap-20 md:p-10 p-4 bg-white text-neutral-800 mt-10">
-      <div className="w-1/6 min-h-72 border-2 rounded p-5 shadow-lg">
+    <div className="flex flex-col sm:flex-row items-start gap-20 md:p-10 p-4 bg-white text-neutral-800 mt-10">
+      <div className="w-full sm:w-1/4 order-1 sm:order-0 min-h-72 border-2 rounded p-5 shadow-lg">
         <h3 className="text-center font-bold border-b pb-5">Veja mais Dicas</h3>
         <ul className="mt-4 space-y-3">
           {otherTips.map((t) => (
@@ -40,9 +40,9 @@ export default async function Page({
         </ul>
       </div>
 
-      <article className="w-full flex flex-col gap-10">
+      <article className="w-full flex flex-col">
         <div>
-          <h1 className="w-full text-xl sm:text-3xl text-center font-bold mb-4 pb-5 border-b">
+          <h1 className="w-full text-center sm:text-start text-xl sm:text-3xl font-bold mb-4 pb-5 border-b">
             {tip.title}
           </h1>
           <div
@@ -55,7 +55,7 @@ export default async function Page({
           />
         </div>
 
-        <div className="w-full border-t mt-10 pb-5 border-neutral-400">
+        <div className="w-full border-t border-neutral-400">
           <div className="flex justify-between items-center p-4 flex-wrap gap-2">
             <span className="text-sm text-neutral-600">
               Publicado por: {tip.user.name}
